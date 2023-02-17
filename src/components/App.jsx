25dfa681f08cb683439edf2d3dd7d36bbe9e5b07
Gubska Nastya <97690198+ngubskaja@users.noreply.github.com>
2023-02-17 20:38:37 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,7 @@ import {Statistics} from "./Statistics/Statistics";
 import {FeedbackOptions} from "./FeedbackOptions/FeedbackOptions";
 import {Notification} from "./Notification/Notification"
 import {Section} from "./Section/Section";
-import css from '../index.css'
+
 
 
 export function App() {
